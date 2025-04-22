eaa404a2021b8c772cf3d649c0f801da43d48e3c
ngdi_ <huuduy.study@gmail.com>
2025-04-23 06:24:04 +0700

    update: websocket controller

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const http = require("http");
-const { WebSocketServer } = require("ws");
 const { mysqlConnection, connectMongoDB } = require("./config/db");
-
 const userRoutes = require("./routes/userRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const WebSocketController = require("./controllers/websocket/WebSocketController");
 
 const app = express();
 const server = http.createServer(app);
@@ -27,69 +26,14 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 
-// Kết nối MongoDB trước khi khởi động server
+// Kết nối MongoDB và khởi động server
 connectMongoDB()
   .then((mongoDB) => {
-    const wss = new WebSocketServer({ noServer: true });
+    const wsController = new WebSocketController(mongoDB);
 
     server.on("upgrade", (request, socket, head) => {
-      const pathname = request.url;
-
-      if (pathname.startsWith("/chat/")) {
-        wss.handleUpgrade(request, socket, head, (ws) => {
-          wss.emit("connection", ws, request);
-        });
-      } else {
-        socket.destroy();
-      }
+      wsController.handleUpgrade(request, socket, head);
     });
-
-    wss.on("connection", (ws, req) => {
-      console.log("Client connected to:", req.url);
-
-      ws.on("message", async (message) => {
-        console.log(`[${req.url}] Received: ${message}`);
-
-        let messageText = message;
-        if (Buffer.isBuffer(message)) {
-          messageText = message.toString();
-        }
-
-        const sender = req.user ? req.user.username : "unknown";
-
-        if (sender === "unknown") {
-          console.error("Không thể lấy username từ request");
-        }
-
-        const messageObj = {
-          roomId: req.url.split("/")[2],
-          text: messageText,
-          sender: sender,
-          timestamp: new Date(),
-        };
-
-        console.log("Message object:", messageObj);
-
-        try {
-          const messagesCollection = mongoDB.collection("messages");
-          await messagesCollection.insertOne(messageObj);
-          console.log("Message saved to MongoDB:", messageObj);
-        } catch (err) {
-          console.error("Error saving message to MongoDB:", err);
-        }
-
-        wss.clients.forEach((client) => {
-          if (client !== ws && client.readyState === 1) {
-            client.send(messageText);
-          }
-        });
-      });
-
-      ws.on("close", () => {
-        console.log("Client disconnected from:", req.url);
-      });
-    });
-
     const port = process.env.PORT || 5000;
     server.listen(port, () => {
       console.log(`Server đang chạy tại: http://localhost:${port}`);
